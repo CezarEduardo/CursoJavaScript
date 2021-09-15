e0f8f18684e4423381e58d0ab07154a9b26fedd8
CezarEduardo <cezareduardo.ce@gmail.com>
2021-09-15 17:48:02 -0300

    Atualizando o arquivo FizzBuzzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,20 @@
+const resultado = FizzBuzz(11)
+
+console.log (resultado)
+
 function FizzBuzz(entrada){
-    
+    if (typeof entrada !== 'number')
+    return 'Não é número';
+
+    if (( entrada % 3 === 0  ) && ( entrada % 5 === 0))
+    return 'FizzBuzz';
+
+    if (entrada % 3 === 0)
+    return 'Fizz';
+
+    if (entrada % 5 === 0)
+    return 'Buzz';
+
+    return entrada;
 }
+
